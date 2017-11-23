68822a302fca48b799653213c2cf06ee4c35c057
Ha Vu <ha.vu@example.com>
2017-11-24 00:13:08 +0700

    pú

---
+++ SoNguyenTo/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SoNguyenTo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCal = new System.Windows.Forms.Button();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtThread = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(28, 195);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(270, 43);
+            this.btnCal.TabIndex = 0;
+            this.btnCal.Text = "Thực hiện";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(121, 31);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(177, 22);
+            this.txtStart.TabIndex = 1;
+            this.txtStart.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Số bắt đầu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số kết thúc";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(121, 77);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(177, 22);
+            this.txtEnd.TabIndex = 4;
+            this.txtEnd.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Số Thread";
+            // 
+            // txtThread
+            // 
+            this.txtThread.Location = new System.Drawing.Point(121, 126);
+            this.txtThread.Name = "txtThread";
+            this.txtThread.Size = new System.Drawing.Size(177, 22);
+            this.txtThread.TabIndex = 6;
+            this.txtThread.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 259);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtThread);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.btnCal);
+            this.Name = "Form1";
+            this.Text = "Tìm số nguyên tố";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtThread;
+    }
+}
+
---
+++ SoNguyenTo/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoNguyenTo
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<int, List<long>> LIST_PRIME;
+        public int THREAD_SUCCESS;
+        public int NUM_THREAD;
+        public Stopwatch SW;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void isAllThreadEnd()
+        {
+            THREAD_SUCCESS++;
+            if (THREAD_SUCCESS == NUM_THREAD)
+            {
+                #region Gop cac list nho lai
+
+                //totalList = totalList.OrderBy(p => p).ToList();
+                List<long> totalList = new List<long>();
+                for (int i = 0; i <= NUM_THREAD; i++)
+                {
+                    if (LIST_PRIME.ContainsKey(i))
+                    {
+                        totalList = totalList.Concat(LIST_PRIME[i]).ToList();
+                    }
+                }
+
+                #endregion
+
+                //SW.Stop();
+                //MessageBox.Show(SW.ElapsedMilliseconds.ToString(), "", MessageBoxButtons.OK);
+
+                string text = "count: " + totalList.Count + "\n\n";
+                foreach (var item in totalList)
+                {
+                    text += " " + item;
+                }
+
+                System.IO.File.WriteAllText("output.txt", text);
+
+                SW.Stop();
+                MessageBox.Show(SW.ElapsedMilliseconds.ToString(), "", MessageBoxButtons.OK);
+
+                btnCal.Invoke(new Action(delegate { btnCal.Enabled = true; }));
+            }
+        }
+
+        public void init(int numThread)
+        {
+            THREAD_SUCCESS = 0;
+            NUM_THREAD = numThread;
+            LIST_PRIME = new Dictionary<int, List<long>>();
+            for (int i = 0; i <= NUM_THREAD; i++)
+            {
+                LIST_PRIME[i] = new List<long>();
+            }
+
+            SW = Stopwatch.StartNew();
+        }
+
+        public void findPrime(List<long> listNum, int index)
+        {
+            if (LIST_PRIME.ContainsKey(index))
+                foreach (var num in listNum)
+                {
+                    if (isPrime(num)) LIST_PRIME[index].Add(num);
+                }
+            isAllThreadEnd();
+        }
+
+        public bool isPrime(long num)
+        {
+            if (num == 2) return true;
+            if (num == 1) return false;
+
+            long limited = (long)Math.Sqrt(num);
+
+            for (long i = 2; i <= limited; i++)
+                if (num % i == 0) return false;
+            return true;
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            #region validate
+
+            string stringNumStart = txtStart.Text;
+            string stringNumEnd = txtEnd.Text;
+            string stringNumThread = txtThread.Text;
+
+            if (stringNumStart == "" || stringNumEnd == "" || stringNumThread == "")
+            {
+                MessageBox.Show("Bạn bắt buộc phải nhập đủ thông tin", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            long numStart;
+            if (long.TryParse(stringNumStart, out numStart) == false)
+            {
+                MessageBox.Show("Số nhập vào chưa đúng định dạng", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            long numEnd;
+            if (long.TryParse(stringNumEnd, out numEnd) == false)
+            {
+                MessageBox.Show("Số nhập vào chưa đúng định dạng", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int numThread;
+            if (int.TryParse(stringNumThread, out numThread) == false)
+            {
+                MessageBox.Show("Số nhập vào chưa đúng định dạng", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (numStart > numEnd)
+            {
+                MessageBox.Show("Số bắt đầu phải nhỏ hơn hoặc bằng số kết thúc", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (numStart < 1)
+            {
+                MessageBox.Show("Số bắt đầu phải là số nguyên dương", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            #endregion
+
+            #region Set trang thai button
+
+            btnCal.Enabled = false;
+
+            #endregion
+
+            #region Tinh toan
+
+            init(numThread);
+
+            #region Them cac so dac biet
+
+            if (numStart <= 2 && 2 <= numEnd) LIST_PRIME[NUM_THREAD].Add(2);
+            if (numStart <= 3 && 3 <= numEnd) LIST_PRIME[NUM_THREAD].Add(3);
+            if (numStart <= 5 && 5 <= numEnd) LIST_PRIME[NUM_THREAD].Add(5);
+            if (numStart <= 7 && 7 <= numEnd) LIST_PRIME[NUM_THREAD].Add(7);
+
+            #endregion
+
+            #region Phan chia cac so vao cac thread
+
+            Dictionary<int, List<long>> dicGroup = new Dictionary<int, List<long>>();
+            for (int i = 0; i < NUM_THREAD; i++)
+            {
+                dicGroup[i] = new List<long>();
+            }
+
+            int sodu = 0;
+            for (long i = numStart; i <= numEnd; i++)
+            {
+                if (i % 6 != 1 && i % 6 != 5) continue;
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) continue;
+                dicGroup[sodu].Add(i);
+                sodu = (++sodu) % NUM_THREAD;
+            }
+
+            for (int i = 0; i < NUM_THREAD; i++)
+            {
+                int j = i;
+                Thread thread = new Thread(() => this.findPrime(dicGroup[j], j));
+                thread.Start();
+            }
+
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
